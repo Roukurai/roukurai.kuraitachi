880050d73b2eb4ab4d3406e301eb5ce109a97583
roukurai <co.roukurai@gmail.com>
2024-01-21 19:14:14 -0600

    Web Compatibility test complete bakemono-legacy-0.0.1 initi commit

--- index.php
+++ index.php
@@ -21,16 +21,26 @@
             <p>This is a placeholder for future content. Check back soon for updates!</p>
             <button onclick="redirectToResume()">View CV</button>
         </section>
+
+        <section>
+            <h2>This is test</h2>
+            <p>This is test as well</p>
+            <button onclick="redirectToGame()">Test Bakemono Legacy 0.0.1</button>
+        </section>
     </div>
 
     <footer>
-        <p>&copy; 2023 Kurai Tachi</p>
+        <p>&copy; 2024 Kurai Tachi</p>
     </footer>
 
     <script>
         function redirectToResume() {
             // Redirect to the resume page (resume.php)
             window.location.href = 'resume.php';
+        }
+
+        function redirectToGame() {
+            window.location.href='/games/bakemono_legacy/0.0.1/index.html';
         }
     </script>
 </body>
